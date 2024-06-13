20aebe3a63c36b28e49db9b7cadf0f40acb2a9ec
Ander Aldabaldetreku <anderaldabaldetreku@gmail.com>
2024-06-13 18:44:12 +0200

    cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
+    images: {
         remotePatterns: [
             {
-                hostname:"pub-0ac36a8b24eb4133942d20338a06e753.r2.dev",
-                protocol:"https",
-                port:"",
+                hostname: "pub-0ac36a8b24eb4133942d20338a06e753.r2.dev",
+                protocol: "https",
+                port: "",
             }
         ]
-    }
+    },
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
